88b174b85244f5b7d07fd7137db067c7f8517720
Weeedos <vit.vosol@gmail.com>
2024-02-25 11:48:22 +0100

    commit 1

--- main.py
+++ main.py
@@ -10,30 +10,32 @@ def send_udp_broadcast(message, port):
 
 
 def main():
-    peer_id = "testing-peer1"
-    port = 9876
+    try:
+        peer_id = "testing-peer1"
+        port = 9876
 
-    while True:
-        query = {"command": "hello", "peer_id": peer_id}
-        query_str = json.dumps(query)
-        send_udp_broadcast(query_str, port)
+        while True:
+            query = {"command": "hello", "peer_id": peer_id}
+            query_str = json.dumps(query)
+            send_udp_broadcast(query_str, port)
 
-        time.sleep(5)
+            time.sleep(5)
 
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.bind(('', port))
-            s.settimeout(5)
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+                s.bind(('', port))
+                s.settimeout(5)
 
-            while True:
-                try:
-                    data, addr = s.recvfrom(1024)
-                    response = json.loads(data.decode('utf-8'))
+                while True:
+                    try:
+                        data, addr = s.recvfrom(1024)
+                        response = json.loads(data.decode('utf-8'))
 
-                    if response.get("peer_id") != peer_id:
-                        print(f"A: {json.dumps(response)}")
-                except socket.timeout:
-                    break
-
+                        if response.get("peer_id") != peer_id:
+                            print(f"A: {json.dumps(response)}")
+                    except socket.timeout:
+                        break
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
     main()
