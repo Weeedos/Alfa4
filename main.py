88856615349cfe83cb2994078b8a50e172e12e5c
Weeedos <vit.vosol@gmail.com>
2024-03-02 14:07:32 +0100

    commit 1

--- main.py
+++ main.py
@@ -36,29 +36,19 @@ def main():
 
                     tcp_peer = TcpPeer(peer_id, port)
                     try:
-                        tcp_socket = tcp_peer.establish_tcp_connection(peer_ip, timeout=5.0)
+                        tcp_socket = tcp_peer.establish_tcp_connection(peer_ip)
                     except TimeoutError:
                         print(f"Connection to {peer_response['peer_id']} timed out. Retrying...")
                         continue
+
                     handshake_response = tcp_peer.perform_handshake(tcp_socket)
 
                     if handshake_response and handshake_response.get('status') == 'ok':
                         print(f"Navázáno trvalé spojení s {peer_response['peer_id']}")
                         chat_history = tcp_peer.messages
                         print(f"Historie chatu s {peer_response['peer_id']}: {chat_history}")
-
-                        """
-                        timestamp = str(int(time.time() * 1000))
-                        message_id = f"{timestamp}"
-                        message = "Test"
-                        tcp_peer.send_chat_message(tcp_socket, message_id, message)
-                        
-
-                        received_message = tcp_peer.receive_chat_message(tcp_socket)
-                        print(f"Přijata nová zpráva od {peer_response['peer_id']}: {received_message}")
-                        """
-
-                        tcp_socket.close()
+                    else:
+                        print(f"Failed to establish connection with {peer_response['peer_id']}. Retrying...")
 
             except socket.timeout:
                 pass
